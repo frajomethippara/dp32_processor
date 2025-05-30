61b0e3281ad28c2eea3dd32cabcd056dbe43dde5
unknown <francisjohn097@gmail.com>
2025-05-30 13:45:00 +0200

    file created

---
+++ dp32_processor.srcs/sources_1/new/dp32_pkg.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/30/2025 01:32:55 PM
+-- Design Name: 
+-- Module Name: dp32_pkg - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity dp32_pkg is
+    Port ( a : in STD_LOGIC;
+           b : out STD_LOGIC);
+end dp32_pkg;
+
+architecture Behavioral of dp32_pkg is
+
+begin
+
+
+end Behavioral;
