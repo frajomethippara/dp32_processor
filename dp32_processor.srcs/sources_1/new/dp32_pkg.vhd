b614c4d908cf14d654ec212f4ee94f4a212da387
unknown <francisjohn097@gmail.com>
2025-11-29 22:20:19 +0100

    Just started

--- dp32_processor.srcs/sources_1/new/dp32_pkg.vhd
+++ dp32_processor.srcs/sources_1/new/dp32_pkg.vhd
@@ -32,39 +32,43 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 package dp32_pkg is
- constant unit_delay : Time := 1 ns;
 
- function bits_to_int (bits : in bit_vector) return integer;
+    constant unit_delay : time := 1 ns;
+    type bool_to_bit_table is array (boolean) of bit;
+    constant bool_to_bit : bool_to_bit_table;
+
+    subtype bit_32 is bit_vector(31 downto 0);
+    type bit_32_array is array(integer range <>) of bit_32;
+
+
+    subtype bit_8 is bit_vector(7 downto 0);
+
+    subtype cc_bits is bit_vector(3 downto 0);
+    subtype cm_bits is bit_vector(3 downto 0);
+
+
+--  constant unit_delay : Time := 1 ns;
+
+--  function bits_to_int (bits : in bit_vector) return integer;
 
 end dp32_pkg;
 
 package body dp32_pkg is 
-    function bits_to_int (bits : in bit_vector) return integer is
-        variable temp : bit_vector(bits'range);
-        variable result : integer := 0;
-        begin
-        if bits(bits'left) = '1' then-- negative number
-        temp := not bits;
-        else
-        temp := bits;
-        end if;
-        for index in bits'range loop-- sign bit of temp = '0'
-        result := result * 2 + bit'pos(temp(index));
-        end loop;
-        if bits(bits'left) = '1' then
-        result := (-result) - 1;
-        end if;
-    return result;
+    -- function bits_to_int (bits : in bit_vector) return integer is
+    --     variable temp : bit_vector(bits'range);
+    --     variable result : integer := 0;
+    --     begin
+    --     if bits(bits'left) = '1' then-- negative number
+    --     temp := not bits;
+    --     else
+    --     temp := bits;
+    --     end if;
+    --     for index in bits'range loop-- sign bit of temp = '0'
+    --     result := result * 2 + bit'pos(temp(index));
+    --     end loop;
+    --     if bits(bits'left) = '1' then
+    --     result := (-result) - 1;
+    --     end if;
+    -- return result;
     end bits_to_int;
 end dp32_pkg;
-
---entity dp32_pkg is
-----  Port ( );
---end dp32_pkg;
-
---architecture Behavioral of dp32_pkg is
-
---begin
-
-
---end Behavioral;
